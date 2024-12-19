8b8baa3356a159a41d41938111dbe52dd377a251
ipekm <ipek.motorcu@ozu.edu.tr>
2024-12-19 12:22:54 +0300

    Updated DML and Listener

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -2,25 +2,26 @@ import java.sql.*;
 import java.util.Properties;
 
 public class DBConnection {
-    final static String url = "jdbc:mysql://localhost:3306/mfa"; //projenin adı.
-    final static String user = "root"; //username and password of mysql
-    final static String password = "Pinar#18";
+    static final String url = "jdbc:mysql://localhost:3306/mfa";
+    static final String user = "root";
+    static final String password = "im66709903";
+
 
 
 
     public static Connection getConnection() throws SQLException {
-/*       Connection myConn = null;
+       Connection myConn = null;
         try{
            myConn = DriverManager.getConnection(url, user, password);
         } catch (Exception e){
             System.out.println("\n>>> ERROR! The Program Can NOT Connect to the Database. <<<");
         }
-       return myConn; //so this returns a Connection object.*/
-        Properties connectionProps = new Properties();
-        connectionProps.put("user", user);
-        connectionProps.put("password", password);
-        connectionProps.put("serverTimezone", "UTC");
-        return DriverManager.getConnection(url, connectionProps);
+       return myConn; //so this returns a Connection object.
+       // Properties connectionProps = new Properties();
+       // connectionProps.put("user", user);
+        //connectionProps.put("password", password);
+        //connectionProps.put("serverTimezone", "UTC");
+        //return DriverManager.getConnection(url, connectionProps);
 
     }
 }
