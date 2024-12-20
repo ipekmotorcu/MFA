649d0eeed57ffc7f7f619ccec4eaae5cbcc9f5b7
basak-kadakal-ozu <basak.kadakal@ozu.edu.tr>
2024-12-20 15:23:00 +0300

    Login duration check added.(MFA 001.1)

--- src/test/java/GUI/LoginViewTest.java
+++ src/test/java/GUI/LoginViewTest.java
@@ -28,4 +28,33 @@ public class LoginViewTest {
         });
     }
 
+    @Test
+    void testLoginDuration() throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            LoginView loginView = new LoginView();
+
+            // Simulate user input (login for 3 users)
+            loginView.userText.setText("listener1");
+            loginView.passwordText.setText("pass123");
+
+            loginView.userText.setText("artist1");
+            loginView.passwordText.setText("pass789");
+
+            loginView.userText.setText("admin1");
+            loginView.passwordText.setText("password123");
+
+
+            long startTime = System.currentTimeMillis();
+            loginView.loginButton.doClick();
+            long endTime = System.currentTimeMillis();
+
+            // Calculate duration
+            long duration = endTime - startTime;
+
+            // Assert that duration is <= 2000ms (2 seconds)
+            assertTrue(duration <= 2000, "Login duration should be less than or equal to 2 seconds.");
+        });
+    }
+
+
 }
